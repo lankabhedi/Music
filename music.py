6395001dce084402342864471e8ff4d96b38e951
KhushiRoy <khushi.roy005@gmail.com>
2021-08-02 12:25:07 +0530

    UI Changes

--- music.py
+++ music.py
@@ -82,39 +82,39 @@ mixer.init()
 
 
 #create the listbox to contain songs
-songs_list=Listbox(root,selectmode=SINGLE,bg="black",fg="white",font=('arial',15),height=12,width=47,selectbackground="gray",selectforeground="black")
+songs_list=Listbox(root,selectmode=SINGLE,bg="#003638",fg="#5E8B7E",font=('arial',15),height=12,width=47,selectbackground="#444444",selectforeground="#F3F2C9")
 songs_list.grid(columnspan=9)
 
 #font is defined which is to be used for the button font 
 defined_font = font.Font(family='Helvetica')
 
 #play button
-play_button=Button(root,text="Play",width =7,command=Play)
+play_button=Button(root,text="Play",bg="#B2AB8C",width =7,command=Play)
 play_button['font']=defined_font
 play_button.grid(row=1,column=0)
 
 #pause button 
-pause_button=Button(root,text="Pause",width =7,command=Pause)
+pause_button=Button(root,text="Pause",bg="#47597E",width =7,command=Pause)
 pause_button['font']=defined_font
 pause_button.grid(row=1,column=1)
 
 #stop button
-stop_button=Button(root,text="Stop",width =7,command=Stop)
+stop_button=Button(root,text="Stop",bg="#DBE6FD",width =7,command=Stop)
 stop_button['font']=defined_font
 stop_button.grid(row=1,column=2)
 
 #resume button
-Resume_button=Button(root,text="Resume",width =7,command=Resume)
+Resume_button=Button(root,text="Resume",bg="#F38BA0",width =7,command=Resume)
 Resume_button['font']=defined_font
 Resume_button.grid(row=1,column=3)
 
 #previous button
-previous_button=Button(root,text="Prev",width =7,command=Previous)
+previous_button=Button(root,text="Prev",bg="#39A6A3",width =7,command=Previous)
 previous_button['font']=defined_font
 previous_button.grid(row=1,column=4)
 
 #nextbutton
-next_button=Button(root,text="Next",width =7,command=Next)
+next_button=Button(root,text="Next",bg="#D79771",width =7,command=Next)
 next_button['font']=defined_font
 next_button.grid(row=1,column=5)
 
